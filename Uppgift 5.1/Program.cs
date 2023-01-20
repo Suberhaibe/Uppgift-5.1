1a5805f0f85af46dd2fcc3c52be93f6393d358fa
Admin <Admin@TEB127008>
2023-01-20 10:43:31 +0100

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hur många antal meningar vill du skriva in");
+        int antal = int.Parse(Console.ReadLine());
+        string[] meningar = new string[antal];
+
+        for (int i = 0; i < antal; i++)
+        {
+            Console.WriteLine("Skriv in valfri mening");
+            meningar[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Här visas meningarna du har skrivit in");
+        for (int i = 0; i < antal; i++)
+        {
+            Console.WriteLine($"\n{meningar[i]}");
+        }
+    }
+}
